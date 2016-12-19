92972c6137f2f0c0ed27b8aaaaee330b2b4c3ce0
Fredy Nurman Saleh <email@fredyns.net>
2016-12-19 14:45:17 +0700

    using kartik/datetime

--- sourcecode/giiant/crud/providers/extensions/DateTimeProvider.php
+++ sourcecode/giiant/crud/providers/extensions/DateTimeProvider.php
@@ -4,21 +4,24 @@ namespace fredyns\suite\giiant\crud\providers\extensions;
 
 class DateTimeProvider extends \schmunk42\giiant\base\Provider
 {
+
     public function activeField($attribute)
     {
         switch (true) {
             case in_array($attribute, $this->columnNames):
-                $this->generator->requires[] = 'zhuravljov/yii2-datetime-widgets';
 
                 return <<<EOS
-\$form->field(\$model, '{$attribute}')->widget(\zhuravljov\widgets\DateTimePicker::className(), [
-    'options' => ['class' => 'form-control'],
-    'clientOptions' => [
-        'autoclose' => true,
-        'todayHighlight' => true,
-        'format' => 'yyyy-mm-dd hh:ii',
-    ],
-])
+\$form
+    ->field(\$model, '{$attribute}')
+    ->widget(\kartik\datetime\DateTimePicker::className(), [
+        'type'          => \kartik\datetime\DateTimePicker::TYPE_INLINE,
+        'pluginOptions' => [
+            'initialDate' => \$model->{$attribute},
+            'format'      => 'yyyy-mm-dd hh:ii',
+            'endDate'     => date('Y-m-d H:i'),
+            'startView'   => 0,
+        ]
+    ])
 EOS;
                 break;
             default:
