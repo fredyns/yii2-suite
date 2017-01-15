81e27333457232c92e05552e4a1c3c89a0dcc068
Fredy Nurman Saleh <email@fredyns.net>
2017-01-15 16:03:45 +0700

    set owner & msg

--- sample/setpermission.sh
+++ sample/setpermission.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+echo "change ownership as yours."
+sudo chown -R $USER ..
+
+echo "change group."
 sudo chgrp -R www-data ..
 sudo chmod -R 775 ..
 
+echo "set runtime folder."
 sudo chgrp -R www-data runtime
 sudo chmod -R 777 runtime
 
+echo "set web assets."
 sudo chgrp -R www-data web/assets
 sudo chmod -R 777 web/assets
 
+echo "done."
